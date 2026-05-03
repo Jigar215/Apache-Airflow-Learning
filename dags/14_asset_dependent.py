28cef8a0f7000b471958d9c4f6fd368122ab9c9a
Jigar215 <prajapatijigar215@gmail.com>
2026-05-03 22:52:46 +0530

    Asset Dependent

---
+++ dags/14_asset_dependent.py
@@ -0,0 +1,20 @@
+from airflow.sdk import dag, task, asset
+import os
+from asset_13 import fetch_data
+
+@asset(
+    schedule=fetch_data,  # This asset depends on the "fetch_data" asset
+    # This is optional but good to include for clarity about the asset's location
+    uri="/opt/airflow/logs/data/data_processed.txt",
+    name="process_data"
+)
+def process_data(self):
+
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(self.uri), exist_ok=True)
+
+    # Simulate data processing by writing to a file
+    with open(self.uri, 'w') as f:
+        f.write(f"Data processed successfully.")
+
+    print(f"Data processed to {self.uri}")
